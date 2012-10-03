ba485dace4d047951622588f79574a909f0aa7b3
ruli-mac <runzhou.li@gmail.com>
2012-10-03 00:51:26 -0400

    I should go to bed

--- BeanBase_Tests.php
+++ BeanBase_Tests.php
@@ -8,12 +8,12 @@ if ( file_exists(TESTS_DIR.'/config/my.db.config.php') ) {
   require_once( TESTS_DIR.'/config/db.config.php' );
 }
 
-require_once( TEST_DIR.'/simpletest/autorun.php' );
-require_once( TEST_DIR.'/vendor/rb.php' );
+require_once( TESTS_DIR.'/simpletest/autorun.php' );
+require_once( TESTS_DIR.'/vendor/rb.php' );
 require_once( __DIR__.'/rbb.php' );
 require_once( __DIR__.'/BaseModel.php' );
 
-require_once( TEST_DIR.'/unit/BaseTestCase.php' );
+require_once( TESTS_DIR.'/unit/BaseTestCase.php' );
 
 class BeanBase_Tests extends TestSuite {
 
@@ -23,13 +23,13 @@ class BeanBase_Tests extends TestSuite {
     // $this->collect( __DIR__.'/unit', new SimplePatternCollector('/_test.php/') );
 
     // RBB
-    $this->addFile( TEST_DIR.'/unit/RBB_Utils.php' );
-    $this->addFile( TEST_DIR.'/unit/RBB_CRUD.php' );
-    $this->addFile( TEST_DIR.'/unit/RBB_Relations.php' );
+    $this->addFile( TESTS_DIR.'/unit/RBB_Utils.php' );
+    $this->addFile( TESTS_DIR.'/unit/RBB_CRUD.php' );
+    $this->addFile( TESTS_DIR.'/unit/RBB_Relations.php' );
 
     // BaseModel
-    $this->addFile( TEST_DIR.'/unit/BaseModel_Utils.php' );
-    $this->addFile( TEST_DIR.'/unit/BaseModel_CRUD.php' );
+    $this->addFile( TESTS_DIR.'/unit/BaseModel_Utils.php' );
+    $this->addFile( TESTS_DIR.'/unit/BaseModel_CRUD.php' );
   }
 
 }
